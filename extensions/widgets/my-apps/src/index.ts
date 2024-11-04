b793781792f82da9c3b6e2b0a649ea9e299d99db
SeverS <sever.abibula@akasha.world>
2024-11-04 14:20:17 +0200

    chore(): remove my-apps widget

--- extensions/widgets/my-apps/src/index.ts
+++
@@ -1,8 +0,0 @@
-import 'systemjs-webpack-interop/auto-public-path';
-import { IntegrationRegistrationOptions, WidgetInterface } from '@akashaorg/typings/lib/ui';
-
-export const register = (opts: IntegrationRegistrationOptions): WidgetInterface => ({
-  mountsIn: opts.layoutSlots?.contextualWidgetSlotId,
-  loadingFn: () => import('./my-apps-widget'),
-  activeWhen: '/@akashaorg/app-extensions/',
-});
