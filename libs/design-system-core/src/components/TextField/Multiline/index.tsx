bcc1537ca954974502ee19a5d8ec1e94e41954cb
Josemaria Nriagu <josemaria.nriagu@akasha.world>
2024-11-07 06:24:40 +0100

    refactor(ds); update text field and story
    Signed-off-by: Josemaria Nriagu <josemaria.nriagu@akasha.world>

--- libs/design-system-core/src/components/TextField/Multiline/index.tsx
+++ libs/design-system-core/src/components/TextField/Multiline/index.tsx
@@ -1,47 +1,48 @@
-import React, { useState } from 'react';
+import React, { forwardRef, useState } from 'react';
+import { apply, tw } from '@twind/core';
+import { MultlineProps } from '../types';
 import EditorMeter from '../../EditorMeter';
 import Stack from '../../Stack';
-import { getContainerClasses } from '../Input/getContainerClasses';
-import { getInputClasses } from '../Input/getInputClasses';
-import { MultlineProps } from '../types';
-import { apply, tw } from '@twind/core';
-import { forwardRef } from 'react';
+import { getContainerClasses } from '../../../utils/getContainerClasses';
+import { getInputClasses } from '../../../utils/getInputClasses';
 import { getRadiusClasses } from '../../../utils/getRadiusClasses';
 
 const MAX_LENGTH = 280;
 
-export const Multiline: React.FC<MultlineProps> = forwardRef(
-  ({ id, status, disabled, radius, ...rest }, ref) => {
-    const [letterCount, setLetterCount] = useState(rest.value?.toString().length);
-    const containerStyle = getContainerClasses(disabled, status);
-    const textAreaStyle = getInputClasses(disabled, status);
-    const radiusStyle = getRadiusClasses(radius);
+export const Multiline: React.FC<MultlineProps> = forwardRef((props, ref) => {
+  const { id, status, disabled, radius, ...rest } = props;
 
-    return (
-      <Stack
-        direction="row"
-        spacing="gap-x-2"
-        padding="pt-1 pb-2"
-        customStyle={`${containerStyle} ${radiusStyle}`}
-      >
-        <textarea
-          ref={ref}
-          aria-labelledby={id}
-          className={tw(apply(`resize-none w-full ${textAreaStyle}`))}
-          maxLength={rest.maxLength ?? MAX_LENGTH}
-          onChange={event => {
-            setLetterCount(event.target.value.length);
-            rest.onChange(event);
-          }}
-          {...rest}
-        />
-        <EditorMeter
-          max={rest.maxLength ?? MAX_LENGTH}
-          value={letterCount}
-          background="grey6"
-          customStyle="ml-auto mt-auto shrink-0"
-        />
-      </Stack>
-    );
-  },
-);
+  // internal state to update letter count as the user types
+  const [letterCount, setLetterCount] = useState(rest.value?.toString().length);
+
+  const containerStyle = getContainerClasses(disabled, status);
+  const textAreaStyle = getInputClasses(disabled, status);
+  const radiusStyle = getRadiusClasses(radius);
+
+  return (
+    <Stack
+      direction="row"
+      spacing="gap-x-2"
+      padding="pt-1 px-2.5 pb-2"
+      customStyle={`${containerStyle} ${radiusStyle}`}
+    >
+      <textarea
+        ref={ref}
+        aria-labelledby={id}
+        className={tw(apply(`resize-none w-full ${textAreaStyle}`))}
+        maxLength={rest.maxLength ?? MAX_LENGTH}
+        onChange={event => {
+          setLetterCount(event.target.value.length);
+          rest.onChange(event);
+        }}
+        {...rest}
+      />
+      <EditorMeter
+        max={rest.maxLength ?? MAX_LENGTH}
+        value={letterCount}
+        background="grey6"
+        customStyle="ml-auto mt-auto shrink-0"
+      />
+    </Stack>
+  );
+});
