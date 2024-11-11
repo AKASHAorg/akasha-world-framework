6bca5a0746168673948827e51e7c076441623f74
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2024-11-11 10:59:23 +0100

    Refactor: TextField and Dropdown (#2478)

--- libs/design-system-core/src/components/TextField/Input/index.tsx
+++ libs/design-system-core/src/components/TextField/Input/index.tsx
@@ -1,45 +1,65 @@
-import React from 'react';
+import React, { forwardRef, useState } from 'react';
+import { apply, tw } from '@twind/core';
+import { InputProps } from '../types';
+import { Color } from '../../types/common.types';
 import Icon from '../../Icon';
 import Stack from '../../Stack';
-import { getContainerClasses } from './getContainerClasses';
-import { getIconClasses } from './getIconClasses';
-import { getInputClasses } from './getInputClasses';
-import { InputProps } from '../types';
-import { apply, tw } from '@twind/core';
-import { forwardRef } from 'react';
-import { getRadiusClasses } from '../../../utils/getRadiusClasses';
+import { getIconClasses } from '../utils/get-icon-classes';
+import { getInputClasses } from '../utils/get-input-classes';
+import { getContainerClasses } from '../utils/get-container-classes';
+import { getRadiusClasses } from '../../../utils';
 
-export const Input: React.FC<InputProps> = forwardRef(
-  (
-    { id, radius, status, iconLeft, iconRight, readOnly, disabled, altBg, fullWidth, ...rest },
-    ref,
-  ) => {
-    const containerStyle = getContainerClasses(disabled, status, readOnly, altBg);
-    const inputStyle = getInputClasses(disabled, status, readOnly);
-    const iconColor = getIconClasses(status, disabled);
-    const radiusStyle = getRadiusClasses(radius);
+export const Input: React.FC<InputProps> = forwardRef((props, ref) => {
+  const { id, radius, status, iconLeft, iconRight, readOnly, disabled, altBg, fullWidth, ...rest } =
+    props;
 
-    return (
-      <Stack
-        direction="row"
-        align="center"
-        spacing="gap-x-2"
-        padding="pt-1 pb-2"
-        fullWidth={fullWidth}
-        customStyle={`${containerStyle} ${radiusStyle}`}
-      >
-        {iconLeft && <Icon icon={iconLeft} color={iconColor} disabled={disabled} />}
-        <input
-          ref={ref}
-          aria-labelledby={id}
-          type="text"
-          className={tw(apply`${inputStyle}`)}
+  // internal state to change icon color on focus/blur
+  const [isFocused, setisFocused] = useState(false);
+
+  const containerStyle = getContainerClasses(disabled, status, readOnly, altBg);
+  const inputStyle = getInputClasses(disabled, status, readOnly);
+  const iconColor = getIconClasses(status, disabled);
+  const radiusStyle = getRadiusClasses(radius);
+
+  const iconFocusColor: Color = { light: 'secondaryLight', dark: 'secondaryDark' };
+
+  const handleFocus = () => setisFocused(true);
+  const handleBlur = () => setisFocused(false);
+
+  return (
+    <Stack
+      direction="row"
+      align="center"
+      spacing="gap-x-2"
+      padding="py-2 px-2.5"
+      fullWidth={fullWidth}
+      customStyle={`${containerStyle} ${radiusStyle}`}
+    >
+      {iconLeft && (
+        <Icon
+          icon={iconLeft}
+          color={isFocused && !status ? iconFocusColor : iconColor}
           disabled={disabled}
-          readOnly={readOnly}
-          {...rest}
         />
-        {iconRight && <Icon icon={iconRight} color={iconColor} disabled={disabled} />}
-      </Stack>
-    );
-  },
-);
+      )}
+      <input
+        ref={ref}
+        type="text"
+        aria-labelledby={id}
+        className={tw(apply`${inputStyle}`)}
+        disabled={disabled}
+        readOnly={readOnly}
+        {...rest}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+      />
+      {iconRight && (
+        <Icon
+          icon={iconRight}
+          color={isFocused && !status ? iconFocusColor : iconColor}
+          disabled={disabled}
+        />
+      )}
+    </Stack>
+  );
+});
