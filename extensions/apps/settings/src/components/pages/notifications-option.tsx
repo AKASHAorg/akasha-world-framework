75efec06b61c72bdbc6ac5807d7bcd0ea8d2a5a8
Klemen Kunstek <klemen.kunstek@akasha.world>
2024-11-13 15:08:12 +0100

    feat(): update settings page for notifications - disabled state (wip)

--- extensions/apps/settings/src/components/pages/notifications-option.tsx
+++ extensions/apps/settings/src/components/pages/notifications-option.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import Stack from '@akashaorg/design-system-core/lib/components/Stack';
 import Text from '@akashaorg/design-system-core/lib/components/Text';
@@ -9,7 +9,10 @@ import Icon from '@akashaorg/design-system-core/lib/components/Icon';
 import { ChevronRightIcon } from '@heroicons/react/24/outline';
 import { ISettingsItem, SettingsOption } from '../../utils/settings-items';
 import ErrorLoader from '@akashaorg/design-system-core/lib/components/ErrorLoader';
+import NotificationSettingsCard from '@akashaorg/design-system-components/lib/components/NotificationSettingsCard';
 import appRoutes, { NOTIFICATIONS } from '../../routes';
+import { NotificationEvents, NotificationTypes } from '@akashaorg/typings/lib/ui';
+import getSDK from '@akashaorg/core-sdk';
 
 const notificationsSettingsItems: ISettingsItem[] = [
   {
@@ -23,13 +26,29 @@ const notificationsSettingsItems: ISettingsItem[] = [
 ];
 
 const NotificationsOption: React.FC = () => {
-  const { baseRouteName, getCorePlugins } = useRootComponentProps();
-  const navigateTo = getCorePlugins().routing.navigateTo;
+  //const sdk = getSDK();
+  const [notificationsEnabled, setNotificationsEnabled] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const { baseRouteName, uiEvents, getCorePlugins } = useRootComponentProps();
   const { t } = useTranslation('app-settings-ewa');
+  const _uiEvents = React.useRef(uiEvents);
+  const navigateTo = getCorePlugins().routing.navigateTo;
+
   const {
     data: { authenticatedDID, isAuthenticating },
   } = useAkashaStore();
   const isLoggedIn = !!authenticatedDID;
+
+  const toastText = {
+    success: {
+      title: t('In-app notifications enabled'),
+      description: t('Notifications for all default apps are enabled. Manage them in preferences.'),
+    },
+    error: {
+      title: t('Couldn’t enable notifications'),
+      description: t('Signature verification failed. Please try again.'),
+    },
+  };
 
   const handleSettingsOptionClick = (option: SettingsOption) => () => {
     return getCorePlugins().routing.navigateTo?.({
@@ -68,39 +87,67 @@ const NotificationsOption: React.FC = () => {
     );
   }
 
+  const handleEnableNotifications = () => {
+    setLoading(true);
+    setNotificationsEnabled(true);
+    // TODO - call SDK API
+
+    const result = true ? 'success' : 'error';
+    _uiEvents.current.next({
+      event: NotificationEvents.ShowNotification,
+      data: {
+        type: NotificationTypes.Success,
+        title: toastText[result].title, // Couldn’t enable notifications
+        description: toastText[result].description,
+      },
+    });
+  };
+
   return (
     <PageLayout title={t('Notifications Settings')}>
-      <Stack padding="p-4">
-        {notificationsSettingsItems.map((item: ISettingsItem, idx: number) => {
-          const baseStyle = `flex py-4 justify-between items-center ${
-            idx !== notificationsSettingsItems.length - 1
-              ? 'border(b-1 solid grey8 dark:grey5)'
-              : 'border-none'
-          }`;
+      {!notificationsEnabled && (
+        <Stack padding="p-4">
+          <NotificationSettingsCard
+            isLoading={loading}
+            handleButtonClick={handleEnableNotifications}
+            text={t('You’ll be prompted with 1 signature')}
+            title={t('Turn on')}
+          />
+        </Stack>
+      )}
+      {notificationsEnabled && (
+        <Stack padding="p-4">
+          {notificationsSettingsItems.map((item: ISettingsItem, idx: number) => {
+            const baseStyle = `flex py-4 justify-between items-center ${
+              idx !== notificationsSettingsItems.length - 1
+                ? 'border(b-1 solid grey8 dark:grey5)'
+                : 'border-none'
+            }`;
 
-          const children = (
-            <>
-              <Text>{`${t('{{itemLabel}}', { itemLabel: item.label as string })}`}</Text>
-              {!item.isSubheading && <Icon icon={<ChevronRightIcon />} accentColor={true} />}
-            </>
-          );
+            const children = (
+              <>
+                <Text>{`${t('{{itemLabel}}', { itemLabel: item.label as string })}`}</Text>
+                {!item.isSubheading && <Icon icon={<ChevronRightIcon />} accentColor={true} />}
+              </>
+            );
 
-          return (
-            <React.Fragment key={`${item.label}`}>
-              {item.clickable && (
-                <Button
-                  plain={true}
-                  customStyle={`w-full ${baseStyle}`}
-                  onClick={handleSettingsOptionClick(item.label)}
-                >
-                  {children}
-                </Button>
-              )}
-              {!item.clickable && <Stack customStyle={baseStyle}>{children}</Stack>}
-            </React.Fragment>
-          );
-        })}
-      </Stack>
+            return (
+              <React.Fragment key={`${item.label}`}>
+                {item.clickable && (
+                  <Button
+                    plain={true}
+                    customStyle={`w-full ${baseStyle}`}
+                    onClick={handleSettingsOptionClick(item.label)}
+                  >
+                    {children}
+                  </Button>
+                )}
+                {!item.clickable && <Stack customStyle={baseStyle}>{children}</Stack>}
+              </React.Fragment>
+            );
+          })}
+        </Stack>
+      )}
     </PageLayout>
   );
 };
