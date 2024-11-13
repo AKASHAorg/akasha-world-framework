fcbeb138036cc9220d9034aed3b31c99e0fdce5d
Klemen Kunstek <klemen.kunstek@akasha.world>
2024-11-13 11:36:43 +0100

    Add pages for notifications settings (#2482)
    * feature(Notifications): Add settings pages
    
    * Add handling of unauthenticated session on settings pages

--- extensions/apps/settings/src/routes.ts
+++ extensions/apps/settings/src/routes.ts
@@ -6,6 +6,9 @@ export const GENERAL = 'General';
 export const APPS = 'Apps';
 export const PLUGINS = 'Plugins';
 export const WIDGETS = 'Widgets';
+export const NOTIFICATIONS = 'Notifications';
+export const PREFERENCES = 'Preferences';
+export const BROWSER_NOTIFICATIONS = 'Browser Notifications';
 export const rootRoute = '/settings';
 
 export default {
@@ -14,4 +17,7 @@ export default {
   [THEME]: `${rootRoute}/theme`,
   [NSFW]: `${rootRoute}/nsfw`,
   [APPS]: `${rootRoute}/apps`,
+  [NOTIFICATIONS]: `${rootRoute}/notifications`,
+  [PREFERENCES]: `${rootRoute}/notifications/preferences`,
+  [BROWSER_NOTIFICATIONS]: `${rootRoute}/notifications/browser-notifications`,
 };
