8a3b7edc8d9113930ab062fe9048a7ccbca67a30
Klemen Kunstek <klemen.kunstek@akasha.world>
2024-11-18 13:18:48 +0100

    chore: remove unused variable

--- extensions/apps/settings/src/components/pages/notifications-browser-option.tsx
+++ extensions/apps/settings/src/components/pages/notifications-browser-option.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import PageLayout from './base-layout';
 import { useAkashaStore, useRootComponentProps } from '@akashaorg/ui-awf-hooks';
