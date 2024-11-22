2467cb1c0cd8f89aba40892ec05d27b36465a908
Klemen Kunstek <klemen.kunstek@akasha.world>
2024-11-22 17:51:26 +0100

    feat: add logic for toggling notifications

--- extensions/apps/settings/src/components/pages/notifications-preferences-option.tsx
+++ extensions/apps/settings/src/components/pages/notifications-preferences-option.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { tw } from '@twind/core';
 import Text from '@akashaorg/design-system-core/lib/components/Text';
@@ -21,9 +21,39 @@ import {
 } from '@akashaorg/design-system-core/lib/components/Icon/akasha-icons';
 import { NotificationEvents, NotificationTypes } from '@akashaorg/typings/lib/ui';
 import Checkbox from '@akashaorg/design-system-core/lib/components/Checkbox';
+import getSDK from '@akashaorg/core-sdk';
+import { UserSettingType } from '@akashaorg/core-sdk/src/common/notification/notification-schemas';
+import { UserSetting } from '@pushprotocol/restapi/src/lib';
+
+const preferencesObjectFactory = (val: boolean): UserSettingType[] => {
+  return [
+    {
+      index: 1,
+      appName: 'Antenna App',
+      enabled: val,
+    },
+    {
+      index: 2,
+      appName: 'Profile App',
+      enabled: val,
+    },
+    {
+      index: 3,
+      appName: 'Vibes App',
+      enabled: val,
+    },
+  ];
+};
+
+// Order of items in array determines the setting category (PushOrg api requirement)
+const DEFAULT_PREFERENCES: UserSettingType[] = preferencesObjectFactory(true);
+const ANTENNA_ARR_INDEX = DEFAULT_PREFERENCES.findIndex(e => e.appName == 'Antenna App');
+const PROFILE_ARR_INDEX = DEFAULT_PREFERENCES.findIndex(e => e.appName == 'Profile App');
 
 const NotificationsPreferencesOption: React.FC = () => {
+  const sdk = getSDK();
   const { theme } = useTheme();
+  const isDarkTheme = theme === 'Dark-Theme';
   const { notificationsEnabled, waitingForSignature, enableNotifications } = useNotifications();
   const { baseRouteName, uiEvents, getCorePlugins } = useRootComponentProps();
   const _uiEvents = React.useRef(uiEvents);
@@ -33,6 +63,26 @@ const NotificationsPreferencesOption: React.FC = () => {
     data: { authenticatedDID, isAuthenticating },
   } = useAkashaStore();
   const isLoggedIn = !!authenticatedDID;
+  
+  const [preferences, setPreferences] = useState<UserSettingType[]>(DEFAULT_PREFERENCES);
+  const [enableAllChecked, setEnableAllChecked] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (notificationsEnabled) {
+      sdk.services.common.notification.getSettingsOfUser().then(preferences => {
+        if (preferences) {
+          setPreferences(preferences);
+        }
+      });
+    }
+  }, [notificationsEnabled]);
+
+  useEffect(() => {
+    setEnableAllChecked(
+      preferences[ANTENNA_ARR_INDEX].enabled && preferences[PROFILE_ARR_INDEX].enabled,
+    );
+  }, [preferences]);
 
   const handleConnectButtonClick = () => {
     navigateTo?.({
@@ -43,6 +93,52 @@ const NotificationsPreferencesOption: React.FC = () => {
         }).toString()}`;
       },
     });
+  };
+
+  const handleUnlockPreferences = async () => {
+    const enabled = await enableNotifications();
+    _uiEvents.current.next({
+      event: NotificationEvents.ShowNotification,
+      data: {
+        type: enabled ? NotificationTypes.Success : NotificationTypes.Error,
+        title: enabled ? t('Notification preferences saved') : t('Couldn’t save preferences'),
+        description: enabled ? undefined : t('Signature verification failed. Please try again.'),
+      },
+    });
+  };
+
+  const handleToggleAll = (val: boolean) => {
+    setPreferences(preferencesObjectFactory(val));
+    setEnableAllChecked(val);
+  };
+
+  const handleSetAntenna = (value: boolean) => {
+    setPreferences(prevState =>
+      prevState.map((item, idx) =>
+        idx === ANTENNA_ARR_INDEX ? { ...item, enabled: value } : item,
+      ),
+    );
+  };
+
+  const handleSetProfile = (value: boolean) => {
+    setPreferences(prevState =>
+      prevState.map((item, idx) =>
+        idx === PROFILE_ARR_INDEX ? { ...item, enabled: value } : item,
+      ),
+    );
+  };
+
+  const handleReset = () => {
+    setPreferences(preferencesObjectFactory(false));
+  };
+
+  const handleSave = async () => {
+    setLoading(true);
+    const preferencesPayload: UserSetting[] = preferences?.map(({ enabled }) => ({
+      enabled,
+    }));
+    await sdk.services.common.notification.setSettings(preferencesPayload);
+    setLoading(false);
   };
 
   if (!isLoggedIn && !isAuthenticating) {
@@ -64,19 +160,6 @@ const NotificationsPreferencesOption: React.FC = () => {
     );
   }
 
-  const handleUnlockPreferences = async () => {
-    const enabled = await enableNotifications();
-    _uiEvents.current.next({
-      event: NotificationEvents.ShowNotification,
-      data: {
-        type: enabled ? NotificationTypes.Success : NotificationTypes.Error,
-        title: enabled ? t('Notification preferences saved') : t('Couldn’t save preferences'),
-        description: enabled ? undefined : t('Signature verification failed. Please try again.'),
-      },
-    });
-  };
-
-  const isDarkTheme = theme === 'Dark-Theme';
   const unlockCard = (
     <Card background={{ light: 'grey9', dark: 'grey3' }} padding="p-3">
       <Stack direction="row" spacing="gap-x-3">
@@ -104,7 +187,6 @@ const NotificationsPreferencesOption: React.FC = () => {
     </Card>
   );
 
-
   return (
     <Stack spacing="gap-y-4" customStyle="mb-2">
       <Text variant="h5">{t('Notification Preferences')}</Text>
@@ -123,8 +205,8 @@ const NotificationsPreferencesOption: React.FC = () => {
                 id="enable-all-notifications-checkbox"
                 value="Enable all"
                 name="enable-all"
-                isSelected={true}
-                handleChange={() => {}}
+                isSelected={enableAllChecked}
+                handleChange={e => handleToggleAll(e.target.checked)}
                 size="large"
                 customStyle="w-6 h-6"
               />
@@ -139,8 +221,8 @@ const NotificationsPreferencesOption: React.FC = () => {
                 id="profile-checkbox"
                 value="Profile"
                 name="profile"
-                isSelected={true}
-                handleChange={() => {}}
+                isSelected={preferences[PROFILE_ARR_INDEX].enabled}
+                handleChange={e => handleSetProfile(e.target.checked)}
                 size="large"
                 customStyle="w-6 h-6"
               />
@@ -163,8 +245,8 @@ const NotificationsPreferencesOption: React.FC = () => {
                 id="antenna-checkbox"
                 value="Antenna"
                 name="antenna"
-                isSelected={true}
-                handleChange={() => {}}
+                isSelected={preferences[ANTENNA_ARR_INDEX].enabled}
+                handleChange={e => handleSetAntenna(e.target.checked)}
                 size="large"
                 customStyle="w-6 h-6"
               />
@@ -197,22 +279,21 @@ const NotificationsPreferencesOption: React.FC = () => {
         {/* Buttons */}
         <Stack direction="row" customStyle="border(t-1 solid grey8 dark:grey5) pt-4 px-3">
           <Button
-            onClick={() => {}}
+            onClick={handleReset}
             variant="text"
             size="md"
             color="dark:secondaryLight secondaryDark"
             label={t('Reset')}
             customStyle="ml-auto"
-            loading={waitingForSignature}
           />
           <Button
-            onClick={() => {}}
+            onClick={handleSave}
             variant="primary"
             size="md"
             color="dark:secondaryLight secondaryDark"
             label={t('Save')}
             customStyle="ml-4"
-            loading={waitingForSignature}
+            loading={loading}
           />
         </Stack>
       </Card>
