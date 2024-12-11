c9091d128a7a7b0a792e07a9751e2097f62ca030
Josemaria Nriagu <josemaria.nriagu@akasha.world>
2024-12-11 20:25:59 +0100

    feat(extensions): update cache after adding new release
    Signed-off-by: Josemaria Nriagu <josemaria.nriagu@akasha.world>

---
+++ extensions/apps/extensions/src/components/pages/extension-release-manager/create-release-mutation-cache.ts
@@ -0,0 +1,33 @@
+import {
+  SetAppReleaseMutation,
+  GetAppsReleasesQuery,
+} from '@akashaorg/typings/lib/sdk/graphql-operation-types-new';
+import { SortOrder } from '@akashaorg/typings/lib/sdk/graphql-types-new';
+import { GetAppsReleasesDocument } from '@akashaorg/ui-awf-hooks/lib/generated/apollo';
+import { ApolloCache } from '@apollo/client';
+
+interface IUpdateCache {
+  cache: ApolloCache<unknown>;
+  document: SetAppReleaseMutation['setAkashaAppRelease']['document'];
+  applicationID: string;
+}
+
+export async function createReleaseMutationCache({ cache, document, applicationID }: IUpdateCache) {
+  cache.writeQuery<GetAppsReleasesQuery>({
+    query: GetAppsReleasesDocument,
+    variables: {
+      first: 10,
+      filters: { where: { applicationID: { equalTo: applicationID } } },
+      sorting: { createdAt: SortOrder.Desc },
+    },
+    data: {
+      akashaAppReleaseIndex: {
+        edges: document.application.releases.edges.map(edge => ({
+          ...edge,
+          node: { ...edge.node, applicationID },
+        })),
+        pageInfo: null,
+      },
+    },
+  });
+}
