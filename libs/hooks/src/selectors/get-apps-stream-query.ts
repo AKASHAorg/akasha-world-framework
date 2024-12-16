b57b27d1852aa75d35c61ca33a480aa46abe1d58
Vali Cotea <quinine35@gmail.com>
2024-12-16 11:41:34 +0200

    chore(): remove redundant code

--- libs/hooks/src/selectors/get-apps-stream-query.ts
+++ libs/hooks/src/selectors/get-apps-stream-query.ts
@@ -2,20 +2,9 @@ import { GetAppsStreamQuery } from '@akashaorg/typings/lib/sdk/graphql-operation
 import { hasOwn } from '../utils/has-own';
 import { isNodeObject } from './selector-utils';
 
-const isAkashaAppStreamEdgeNode = (data: GetAppsStreamQuery) => {
-  return (
-    data &&
-    'node' in data &&
-    'akashaAppStreamList' in data.node &&
-    typeof data.node['akashaAppStreamList'] === 'object' &&
-    'edges' in data.node.akashaAppStreamList &&
-    Array.isArray(data.node.akashaAppStreamList.edges)
-  );
-};
-
 export const selectAkashaAppStreamStatus = (data: GetAppsStreamQuery) => {
   if (
-    isAkashaAppStreamEdgeNode(data) &&
+    hasOwn(data, 'node') &&
     hasOwn(data.node, 'akashaAppsStreamList') &&
     isNodeObject(data.node.akashaAppsStreamList.edges[0]) &&
     hasOwn(data.node.akashaAppsStreamList.edges[0].node, 'status')
